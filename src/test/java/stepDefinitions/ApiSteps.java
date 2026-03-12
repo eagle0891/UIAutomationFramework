44cf948cc64c0774a43722631ffeab4016c9bac8
Ugo Agwo <upa01@hotmail.co.uk>
2026-03-12 21:15:38 +0000

    use a different endpoint

--- src/test/java/stepDefinitions/ApiSteps.java
+++ src/test/java/stepDefinitions/ApiSteps.java
@@ -9,26 +9,19 @@ import static org.junit.Assert.*;
 public class ApiSteps {
     private Response response;
     private RequestSpecification request;
-    private static final String BASE_URL = "https://reqres.in/api";
+    // Updated BASE_URL
+    private static final String BASE_URL = "https://jsonplaceholder.typicode.com";
 
     @Given("I prepare a request for the users endpoint")
     public void i_prepare_a_request() {
         request = RestAssured.given()
-                .header("Content-Type", "application/json")
-                .header("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8")
-                .header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36");
+                .header("Content-Type", "application/json");
     }
 
     @When("I send a GET request to fetch user with ID {int}")
-    public void i_send_a_get_request(int userId) throws InterruptedException {
+    public void i_send_a_get_request(int userId) {
+        // This API is very stable, so we can simplify the request
         response = request.get(BASE_URL + "/users/" + userId);
-
-        // If we get a 403, wait and retry once
-        if (response.getStatusCode() == 403) {
-            System.out.println("Got a 403, retrying in 2 seconds...");
-            Thread.sleep(2000);
-            response = request.get(BASE_URL + "/users/" + userId);
-        }
     }
 
     @Then("the API status code should be {int}")
@@ -38,7 +31,7 @@ public class ApiSteps {
 
     @Then("the response body should contain the email {string}")
     public void the_response_body_should_contain_email(String expectedEmail) {
-        String actualEmail = response.jsonPath().getString("data.email");
+        String actualEmail = response.jsonPath().getString("email");
         assertEquals("Email mismatch!", expectedEmail, actualEmail);
         System.out.println("Validated User Email: " + actualEmail);
     }
--- src/test/resources/features/UserApi.feature
+++ src/test/resources/features/UserApi.feature
@@ -5,4 +5,4 @@ Feature: User API Validation
     Given I prepare a request for the users endpoint
     When I send a GET request to fetch user with ID 2
     Then the API status code should be 200
-    And the response body should contain the email "janet.weaver@reqres.in"
+    And the response body should contain the email "Shanna@melissa.tv"
